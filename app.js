327bbf912514eab58e2a02cdf82111619967c40d
Myles Javillonar <152746324+mylesjav@users.noreply.github.com>
2024-01-28 00:52:43 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var winEl = document.querySelector("")
-var lossesEl = document.querySelector("")
-var timerEl = document.querySelector("")
-var startGameButtonEl = document.querySelector("")
-var gameBoardEl = document.querySelector("")
-var gameResultEl = document.querySelector("")
-var gameDisplayEl = document.querySelector("")
-var controlsEl = document.querySelector("")
+var winEl = document.querySelector(".win")
+var lossesEl = document.querySelector(".losses")
+var timerEl = document.querySelector(".timer")
+var startGameButtonEl = document.querySelector(".startGameButton")
+var gameBoardEl = document.querySelector(".gameboard")
+var gameResultEl = document.querySelector(".gameResult")
+var gameDisplayEl = document.querySelector(".gameDisplay")
+var controlsEl = document.querySelector(".controls")
 
 var wins = 0;
 var losses = 0;
 var timer = null;
 var timeLeft = 0;
-var currentWordIndex:
-var currentGuess =[];
+var currentWordIndex;
+var currentGuess = [];
 
 var KWordList = [
     "variable",
@@ -22,7 +22,8 @@ var KWordList = [
 ];
 
 var kDuraion = 20;
-var kStorageKey   = "week-4-activity-28-scores"
+var kStorageKey   = "week-4-activity-28-scores";
+var kAllowedKeys = "abcdefghijklmnopqrstuvwxyz";
 
 //Event - page load
 function init() {
@@ -30,7 +31,7 @@ function init() {
 
 
 // retrieve data
-    var scores = JSON.parse(localStorage.getItem("kStorageKey"))
+    var scores = JSON.parse(localStorage.getItem(kStorageKey))
 
 // update state    
     if(scores) {
@@ -50,9 +51,9 @@ if (!timer) {
     // start a timer
     timer = setInterval(handleTimerTick, 1000)
     // choose a word from list
-    currentWordIndex = math.floor(math.random() * KWordList.lenth);
+    currentWordIndex = math.floor(math.random() * kWordList.lenth);
     // set current guess
-    currentGuess = new Array(KWordList[currentWordIndex].length).fill("_");
+    currentGuess = new Array(kWordList[currentWordIndex].length).fill("_");
 }
 
     // hide the start button
@@ -75,7 +76,7 @@ hideElement(gameResultEl);
   
 
 
-}
+} 
 startGameButtonEl.addEventListener("click", handleClickStart)
 //Event - timer tick
 function handleTimerTick(ev) {
@@ -91,6 +92,28 @@ if(timeLeft === 0 ) {
 //Event - type letter
 function handleKeyDown(ev) {
 console.log("key pressed:", ev.key);
+
+if(timer && kAllowedKeys.includes(ev.key)) {
+    if(!currentGuess.includes(ev.key) && 
+     KWordLis[currentWordIndex].includes(ev.key)
+     ) {
+        currentGuess = KWordList[currentWordIndex]
+        .split("")
+        .map(function (letter, index) {
+            if (letter === ev.key) {
+                return letter;
+            }
+            return currentGuess[index];
+        });
+     }
+     // update UI
+     updateWordDisplay();
+
+     if(currentGuess.join("") === KWordList[currentWordIndex]) {
+        handleGameEnds(true);
+     }
+
+    }
 }
 document.addEventListener("keydown", handleKeyDown);
  
@@ -112,7 +135,7 @@ document.addEventListener("keydown", handleKeyDown);
     //Update UI
 
     // display result message
-    updateScoreboard
+    
 function updateScoreboard() {
     //Update UI
     winsEl.textContent = wins;
@@ -128,17 +151,22 @@ function showElement(el) {
 } 
 
 function displayResult(didWin) {
-    gameResultEl.classList.remove = "success";
-    gameResultEl.classList.remove = "failure";
+    gameResultEl.classList.remove("success"); 
+    gameResultEl.classList.remove("failure");
     hideElement(timerEl);
         
         if(didWin) {
             gameResultEl.textContent = "You Win!";
-            gameResultEl.classList.add = "success";
+            gameResultEl.classList.add("success");
         } else {
             gameResultEl.textContent = "You Lost!";
-            gameResultEl.classList.add = "failure";
+            gameResultEl.classList.add("failure");
         }
+        showElement(gameResultEl);
+
+    function updateWordDisplay(){
+        gameDisplayEl.textContent = currentGuess.join(" ");
+    }
 }
 
 // Game start
